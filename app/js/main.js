745cfbc3b506a71c19a6191ff25852b65049948c
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-07-07 17:28:15 -0300

    nice

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,7 @@
-var dollars = new Decimal(1.2);
+var dollars = new Decimal(1.001);
 var dollar_mulit = new Decimal(1.15)
 
 setInterval(() => {
     dollars = dollars.mul(1.15)
+    document.getElementById("dollars").innerHTML = "You Have " + dollars + " Dollars."
 }, 100);
